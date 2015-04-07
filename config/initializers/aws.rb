d31848137256b5e377a2d6f66025f1fcafaf5c02
Pierre Noizat <noizat@hotmail.com>
2015-04-07 15:56:23 +0200

    Fixed aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
 AWS.config({
-    :access_key_id => Figaro.env.access_key_id,
-    :secret_access_key => Figaro.env.secret_access_key
+    :access_key_id => Figaro.env.aws_access_key_id,
+    :secret_access_key => Figaro.env.aws_secret_access_key
   })
